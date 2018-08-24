6207c9521693b9abcec1306073a796bb26a7fe5f
nathias <n@th1.as>
2018-08-25 00:45:59 +0200

    binary search file created

---
+++ binarySearch.js
@@ -0,0 +1,15 @@
+function binarySearch(list, value) {
+
+}
+
+var yellowPages = ['Apple Store', 'Bags Galore', 'Bike Store', 'Donuts R Us', 'Eat a Lot', 'Good Food', 'Pasta Place', 'Pizzeria', 'Ruby Lounge', 'Zooper'];
+binarySearch(yellowPages, 'Pizzeria');                   // 7
+binarySearch(yellowPages, 'Apple Store');                // 0
+
+binarySearch([1, 5, 7, 11, 23, 45, 65, 89, 102], 77);    // -1
+binarySearch([1, 5, 7, 11, 23, 45, 65, 89, 102], 89);    // 7
+binarySearch([1, 5, 7, 11, 23, 45, 65, 89, 102], 5);     // 1
+
+binarySearch(['Alice', 'Bonnie', 'Kim', 'Pete', 'Rachel', 'Sue', 'Tyler'], 'Peter');  // -1
+binarySearch(['Alice', 'Bonnie', 'Kim', 'Pete', 'Rachel', 'Sue', 'Tyler'], 'Tyler');  // 6
+
